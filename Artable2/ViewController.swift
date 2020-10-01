c145338810c86f14ea9022459c6ffb92163be537
Warren Hansen <whansen1@mac.com>
2020-10-01 14:09:41 -0700

    Initial Commit

---
+++ Artable2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Artable2
+//
+//  Created by Warren Hansen on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
